57e82dd6578dcdd611e07c23bdb53c48ad9b749b
ertkonsert <martaaagren@gmail.com>
2020-10-13 10:16:24 +0200

    first commit

---
+++ president/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace president
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
